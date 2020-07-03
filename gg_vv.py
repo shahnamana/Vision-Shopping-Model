24dabc743940678e4799cbe814cd33488a5bee03
shahnamana <naman.as@somaiya.edu>
2020-07-03 22:01:06 +0530

    Vision API script added

---
+++ gg_vv.py
@@ -0,0 +1,33 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.abspath('mushrshirt.png')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+
+
+l = []
+
+color = ['Pink', 'Red', 'Brown', 'Black', 'White', 'Yellow', 'Blue', 'Green', 'Grey', 'Purple', 'Orange', 'Maroon']
+
+for label in labels:
+    print(label.description)
+    l.append(label.description)
